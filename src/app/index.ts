14f319e0b37fbdfe1ca4dc4bcce52cced6332894
Teresa Romero <romero.teresa@protonmail.com>
2025-02-14 13:00:06 +0100

    add OpenAPI documentation and integrate Swagger UI for development
    Signed-off-by: Teresa Romero <romero.teresa@protonmail.com>

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,17 @@
+import path from 'path';
+
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  const openapiFile: swaggerUi.JsonObject = YAML.load(
+    path.join(__dirname, '../../openapi.yaml'),
+  );
+
+  app.use('/openapi', swaggerUi.serve, swaggerUi.setup(openapiFile));
+}
 
 export default app;
