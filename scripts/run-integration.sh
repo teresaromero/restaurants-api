c1dbf644593e76444a50a8a980d777777528416e
Teresa Romero <romero.teresa@protonmail.com>
2025-02-16 15:48:43 +0100

    fix: update integration test script to create test database correctly
    Signed-off-by: Teresa Romero <romero.teresa@protonmail.com>

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -2,21 +2,12 @@
 
 set -e
 
-DATABASE_FILE="./prisma/test.db"
-export DATABASE_URL="file:test.db"
+export DATABASE_URL="file:./test.db"
 
-if [ -f "$DATABASE_FILE" ]; then
-  echo "Removing existing test database..."
-  rm "$DATABASE_FILE"
-fi
-
-echo "Setting up test database..."
-yarn prisma:push:integration
+echo "Creating test database..."
+yarn prisma:push
 
 echo "Running integration tests..."
 yarn test:integration
-
-echo "Removing test database..."
-rm -rf "$DATABASE_FILE"
 
 echo "Integration tests completed successfully."
